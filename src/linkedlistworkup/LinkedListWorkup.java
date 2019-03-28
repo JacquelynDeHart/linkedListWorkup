9d7b0a3f2089f78a5058fefbb7df2ccb8ae0e4b5
jacquelyn0890@gmail.com <jacquelyn0890@gmail.com>
2019-03-27 21:58:44 -0500

    first commit

---
+++ src/linkedlistworkup/LinkedListWorkup.java
@@ -0,0 +1,26 @@
+/**
+ * This program will make use of and implement a LinkedList with methods for
+ * adding data to the list, remove an item by position, remove an item by value,
+ * clear all elements from a list, get an item based on the position, and output
+ * the contents of a list.
+ */
+package linkedlistworkup;
+
+/**
+ *
+ * @author Jacquelyn Johnson
+ * Course: COSC 2336-2803
+ * Date: March 27, 2019
+ */
+public class LinkedListWorkup {
+    
+    
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
